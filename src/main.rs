0bb46a4dc8c728fea3420a11e572fef22f261f91
bahrom04 <magdiyevbahrom@gmail.com>
2025-06-24 20:30:45 +0500

    chore: todo tasks for completing tests and quote struct redesign

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,23 @@ use serde;
 use rand::seq::IndexedRandom;
 use std::env;
 use std::fs::File;
+use std::path::PathBuf;
 
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 struct Quote {
     author: String,
     quotes: Vec<String>,
 }
 
 impl Quote {
-    fn read_json(path: &str) -> Result<Vec<Quote>, CustomError> {
+    fn new(author: String, quotes: Vec<String>) -> Self {
+        Self {
+            author, quotes
+        }
+    }
+
+    fn read_json(path: PathBuf) -> Result<Vec<Quote>, CustomError> {
         let mut exe_path = env::current_exe()?;
         exe_path.pop();
         exe_path.push(path);
@@ -36,21 +43,24 @@ impl Quote {
         Ok(quotes)
     }
 
-    fn get_quote(target_author: &str, quotes: Vec<Quote>) -> Option<Quote> {
-        for quote in quotes {
-            if target_author == quote.author {
-                return Some(quote);
-            }
+    fn get_quote(&self, selector: usize) -> Option<String> {
+        
+        if selector > self.quotes.len() {
+            return None;
         }
-        None
+
+        Some(
+            self.quotes.get(selector)
+        )
     }
 }
 
 fn main() {
     let args = Cli::parse();
-    let path = "quotes.json";
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("./quotes.json");
 
-    match Quote::read_json(&path) {
+    match Quote::read_json(path) {
         Ok(quotes) => {
             match Quote::get_quote(&args.author, quotes) {
                 Some(quote) => {
@@ -66,4 +76,28 @@ fn main() {
         }
         
     }   
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn read_json_method(){
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("./test.json");
+        let quotes = Quote::read_json(path).unwrap();
+
+        let test_quote_data_1: Quote = Quote{
+            author: "bahrom04".to_string(), 
+            quotes: vec!["oʻzbekchasi yoʻq ekan".to_string(), "asahi oʻrnatsammikan?".to_string()] 
+        };
+        let test_quote_data_2: Quote = Quote{
+            author: "orzklv".to_string(), 
+            quotes: vec!["cooked".to_string(), "koʻkaldosh".to_string()] 
+        };
+
+        assert_eq!(quotes, vec![test_quote_data_1, test_quote_data_2])
+
+    }
 }
